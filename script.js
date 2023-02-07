1a983834f2a017eb55a0ff6a05c23e6db5d26f3e
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 18:12:30 +0300

    br_8

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-let a = 1 + 2 + 3;
-alert(a);
+"use strict";
+let a, b, c, d; 
+
+a = 10;
+b = 5;
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a/b);
+
+
+
+c = 10;
+d = 5;
+result = c + d;
+alert(result);
+
+
+a = 2;
+b = 4;
+c = 5;
+result = a + b + c;
+alert(result);
+
+
+a = 20;
+b = 5;
+c = a - b;
+d = 7;
+result = c + d;
+alert(result);
