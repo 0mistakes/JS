d0661ecc5be5c1b1dd5e7690061644fdd0be4148
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 19:00:14 +0300

    17 quest

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let str = 'abracadabra';
-alert(str.length);
+let str1 = 'xxx';
+let str2 = 'yyy';
+let txt  = `aaa  ${str1}  bbb  ${str2}  ccc`;
+alert(txt);
+
+// 2
+str = `a
+b
+c`;
+alert(str);
