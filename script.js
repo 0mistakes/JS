abccff00f2d3153d40bf9d352fc91c1bfc4ba3db
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 18:56:31 +0300

    15 quest

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let str = 'Владимир';
-let str1 = "Олейник";
+let a, b, c, d, result, str; 
+
+str = "!!!"
 alert(str);
-alert(str1);
+a="java";
+b="script";
+alert(a + b);
+c="bebra";
+d="amogus";
+alert(c + ' ' + d);
