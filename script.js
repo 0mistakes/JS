f68cd9beab2334d2e2ce79dc7804361f2f02bc88
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 19:07:26 +0300

    br_19

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-let a;
-alert(a); // выведет undefined
-a = null;
-alert(a);
-a = true;
-alert(a);
-a = false;
-alert(a);
-
-let str1 = 'abc';
-let str2 = 'tyu';
-alert(str1 * str2);
-
-let b = 10;
-let c = 0;
-alert(b / c);
-
-b = -10;
-c = 0;
-alert(b/c);
+let num = 123;
+console.log(num);
+//2
+let num1 = 123;
+let num2 = 456;
+let num3 = 567;
+console.log(num1, num2, num3);
+//3
+console.log(123);
+console.log('123');
+console.log(1.23);
+//4
+alert(eee);
