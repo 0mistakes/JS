2676439120534ec4142baee9813d12bddec14623
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 18:57:26 +0300

    16 quest

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-let a, b, c, d, result, str; 
-
-str = "!!!"
-alert(str);
-a="java";
-b="script";
-alert(a + b);
-c="bebra";
-d="amogus";
-alert(c + ' ' + d);
+let str = 'abracadabra';
+alert(str.length);
