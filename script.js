630ffd4d859c2ad862b14a2f86bf38ee5967cb21
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 19:46:11 +0300

    25 quest

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let num1 = 5;
-let num2 = 6;
-
-alert(String(num1) + String(num2));
-
-let num = 12345;
-let str = String(num); // преобразуем наше число к строке
-alert(str.length);
-
-num = 12345;
-num2 = 12446;
-alert(String(num).length); // найдем длину строки
-alert(String(num2).length);
+alert(true + 3);
+alert(true + true);
+alert(true - true);
+alert(true + false);
+alert('1' + true);
+alert( String(true) + 1 );
+alert( String(true) + Number(true) );
