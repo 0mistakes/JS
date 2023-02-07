37e652f914928dc10b352291a4fda59d75c64e6c
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 19:07:26 +0300

    br_19

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-let num = 123;
-console.log(num);
-//2
-let num1 = 123;
-let num2 = 456;
-let num3 = 567;
-console.log(num1, num2, num3);
-//3
-console.log(123);
-console.log('123');
-console.log(1.23);
-//4
-alert(eee);
+const pi = 3.14;
+let dlinaokr = pi * 2;
+console.log(dlinaokr)
