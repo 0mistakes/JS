7ef0113abcbb87c17ddaea05299f6e4c80ef2c0b
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 18:25:04 +0300

    9 quest

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-"use strict";
-let a, b, c, d; 
-
-a = 10;
-b = 5;
-alert(a+b);
-alert(a-b);
-alert(a*b);
-alert(a/b);
-
-
-
-c = 10;
-d = 5;
-result = c + d;
-alert(result);
-
-
-a = 2;
-b = 4;
-c = 5;
-result = a + b + c;
-alert(result);
-
-
-a = 20;
-b = 5;
-c = a - b;
-d = 7;
-result = c + d;
-alert(result);
+let a = 5 + 5 * 3;
+alert(a);
+//выведется 20
+a = 5 + 5 * 3 + 3;
+alert(a);
+//выведется 23
+a = 8 / 2 + 2;
+alert(a);
+//выведется 6
+a = 8 + 2 / 2;
+alert(a);
+//выведется 8
+a = 8 / 2 * 2;
+alert(a);
+//выведется 8
+a = 8 * 4 / 2 / 2;
+alert(a);
+//выведется 8
+a = (2 + 3) * (2 + 3);
+alert(a);
+//выведется 25
+a = (2 + 3) * 2 + 3;
+alert(a);
+// выведется 13
+a = 2 * (2 + 4 * (3 + 1));
+alert(a);
+//выведется 48
+a = 2 * 8 / 4;
+alert(a);
+//выведется 4
+a = (2 * 8) / 4;
+alert(a);
+// выведется 4
+a = 2 * (8 / 4);
+alert(a);
+// выведется 4
