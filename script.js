f5d4569ef8c4a40bcb163f3d4be5b05a6e984829
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-07 19:39:46 +0300

    24 quest

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let a = '5px';
-let b = '6px';
-alert(parseInt(a) + parseInt(b));
-a = '5.5px';
-b = '6.25px';
-alert(parseFloat(a) + parseFloat(b));
-a = '5.5px';
-b = '6.25px';
-alert(parseFloat(a) + parseFloat(b) + 'px');
+let num1 = 5;
+let num2 = 6;
+
+alert(String(num1) + String(num2));
+
+let num = 12345;
+let str = String(num); // преобразуем наше число к строке
+alert(str.length);
+
+num = 12345;
+num2 = 12446;
+alert(String(num).length); // найдем длину строки
+alert(String(num2).length);
