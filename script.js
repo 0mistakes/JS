baa49580e457951ab955613a69b1032b3619d6f3
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-15 20:04:51 +0300

    34 quest

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-let age = prompt('Введите свой возраст');
-alert('Ваш возраст: ' + age);
+document.write('Привет, мир!');
 
-let num1 = Number(prompt('Введите первое число'));
-let num2 = Number(prompt('Введите второе число'));
-let sum = num1 + num2;
-alert(sum);
+document.write('<em>Курсивный текст</em>');
 
-let side = Number(prompt('Введите длину стороны квадрата'));
-let area = side ** 2;
-alert('Площадь квадрата: ' + area);
+let str = 'text';
+document.write('<em>' + str + '</em>');
 
-let length = Number(prompt('Введите длину прямоугольника'));
-let width = Number(prompt('Введите ширину прямоугольника'));
-let perimeter = 2 * (length + width);
-alert('Периметр прямоугольника: ' + perimeter);
+for (let i = 1; i <= 5; i++) {
+    document.write(i + '<br>');
+}
