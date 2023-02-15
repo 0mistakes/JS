f5d2c4dd4ec6b0596b8ac562c41d659a3f6b08a1
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-15 19:55:13 +0300

    br_30

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-let number = 12345;
-let sum = 0;
-while (number) {
-  sum += number % 10;
-  number = Math.floor(number / 10);
-}
-console.log("Сумма цифр числа 12345: " + sum);
-
-let product = 1;
-while (number) {
-  product *= number % 10;
-  number = Math.floor(number / 10);
-}
-console.log("Произведение цифр числа 12345: " + product);
-
-let reversed = 0;
-while (number) {
-  reversed = reversed * 10 + number % 10;
-  number = Math.floor(number / 10);
-}
-console.log("Число 12345 в обратном порядке: " + reversed);
+let num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
