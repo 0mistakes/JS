b0f463de49377e668756636a2c5dee33bfebe7d4
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-15 20:26:26 +0300

    44 quest

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let arr = [];
-
-arr[3] = "a";
-arr[8] = "b";
-document.write(`Длина: ${arr.length}<br>Массив: ${arr}`);
+let mass = [];
+for (let index = 0; index < 3; index++) {
+  arr.push(index + 1);
+}
+document.write(`Задание 1: ${mass}<br>`);
+for (let index = 3; index < 5; index++) {
+  arr.push(index + 1);
+}
+document.write(`Задание 2: ${mass}`);
