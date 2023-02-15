3d9be4cc7a02319bea88579c510ee4e4ec7ef239
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-15 20:32:14 +0300

    50 quest

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-let obj = {
-    1: "January",
-    2: "February",
-    3: "March",
-    4: "April",
-    5: "May",
-    6: "June",
-    7: "July",
-    8: "August",
-    9: "September",
-    10: "October",
-    11: "November",
-    12: "December",
-  };
-  for (let index = 0; index < 12; index++) {
-    console.log(obj[index + 1]);
-  }
+let user = { name: "Bebr", surname: "Bebrov", patronymic: "Bebrovich" };
+document.write(`${user.name} ${user.surname} ${user.patronymic}`);
