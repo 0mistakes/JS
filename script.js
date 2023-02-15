00f646b2a90a4d6c83c0280576bdc89c4c6251b9
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-15 20:33:30 +0300

    51 quest

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let user = { name: "Bebr", surname: "Bebrov", patronymic: "Bebrovich" };
-document.write(`${user.name} ${user.surname} ${user.patronymic}`);
+
+"use strict";
+
+let today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0");
+var yyyy = today.getFullYear();
+console.log(yyyy);
+let date = { year: yyyy, month: mm, day: dd };
+document.write(`${date.year}-${date.month}-${date.day}`);
