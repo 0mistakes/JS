94ebb7dafe9c4af6fa90e92e603a654cba750478
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-22 21:58:37 +0300

    79 quest

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 "use strict";
 
-let test1 = '123';
-let test2 = 123;
-
+let test1 = '3';
+let test2 = '3';
+		
 if (test1 == test2) {
 	console.log('+++');
 } else {
-	console.log('---');
+	console.log('---');   true
 }
+test1 = '3';
+test2 = '3';
+		
+if (test1 === test2) {
+	console.log('+++');
+} else {
+	console.log('---');   true
+}
+test1 = 3;
+test2 = '3';
+		
+if (test1 == test2) {
+	console.log('+++');
+} else {
+	console.log('---'); // true
+}
+test1 = 3;
+test2 = '3';
+		
+if (test1 === test2) {
+	console.log('+++');
+} else {
+	console.log('---');   false
+}
+test1 = 3;
+test2 = 3;
+		
+if (test1 === test2) {
+	console.log('+++');
+} else {
+	console.log('---');   true
+} 
