4a50845481be190b41f2318b77b3e97d6d927776
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-02-22 21:44:47 +0300

    67 quest

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 "use strict";
 
-console.log( Array.isArray([1, 2, 3]) );
-// true
-console.log( Array.isArray({x: 1, y: 2, z: 3}) );
-// false
+let test = {x: 1, y: 2, z: 3};
+console.log(test);
+//object
+test = {x: 1, y: 2, z: 3};
+console.log(test.x);
+//primitive
+test = [1, 2, 3];
+console.log(test);
+//object
+test = [1, 2, 3];
+console.log(test[1]);
+//primitive
+test1 = [1, 2, 3];
+test2 = 1;
+console.log(test1);
+//object
+test1 = [1, 2, 3];
+test2 = 1;
+console.log(test1[test2]);
+//primitive 
