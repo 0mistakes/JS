25771753a747a3d7a8e7aebd3a45789e41183973
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-06-18 23:05:52 +0300

    159

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function num(a, b, c) {
-  return console.log(a + b + c);
-}
+let param1 = 1;
+let param2 = 5;
+let param3 = 3;
 
-num(1, 2, 3);
+function num(a, b, c) {
+   return console.log(a + b + c);
+};
+num(param1, param2, param3);
