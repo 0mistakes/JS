55983c0bd09c09c3d71eb0c891f2b3306edb17f7
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-06-19 00:22:59 +0300

    343

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-let elems = document.querySelectorAll('p');
+let elem = document.querySelector('#elem');
+let show = document.querySelector('#show');
+let hide = document.querySelector('#hide');
 
-for (let elem of elems) {
-  elem.addEventListener('click', function func() {
-    let input = document.createElement('input');
-    input.value = elem.textContent;
+hide.addEventListener('click', function() {
+	elem.classList.add('hidden');
+});
 
-    elem.textContent = '';
-    elem.appendChild(input);
-
-    input.addEventListener('blur', function () {
-      elem.textContent = this.value;
-      elem.addEventListener('click', func);
-    });
-
-    elem.removeEventListener('click', func);
-  });
-}
+show.addEventListener('click', function() {
+	elem.classList.remove('hidden');
+});
