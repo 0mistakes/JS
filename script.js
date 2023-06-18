7505c160bae9058398c28329d0b366158e03e7fe
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-06-18 22:29:24 +0300

    101

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 "use strict";
 
-let a = 2 * (3 - 1);
-let b = 6 - 2;
-document.write(a == b, '<br>')
+let age = 17;
+let adult;
 
-let c = 5 * (7 - 4);
-let d = 1 + 2 + 7;
-document.write(c > d, '<br>')
-
-let e = 2 ** 4;
-let f = 4 ** 2;
-document.write(e != f)
-
-
-"use strict";
-
-let ok = confirm('Вам есть 18 лет?');
-
-if (ok) {
-	document.write('текст для взрослых 0_0');
+if (age >= 18) {
+	adult = true;
 } else {
-	document.write('доступ запрещен');
+	adult = false;
 }
+
+document.write(adult);
