647d0c38910cfcffcedda13cb537a9b0fa84d488
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-06-18 23:20:41 +0300

    184

--- script.js
+++ script.js
@@ -1,91 +1,16 @@
-function func1() {
-  console.log('!');
+let newobj = {
+  func1: function () { return 1 },
+  func2: function () { return 2 },
+  func3: function () { return 3 },
 };
 
-
-
-
-let func2 = function () {
-  console.log('!');
-};
-
-
-
-
-+function () {
-  console.log('!');
-};
+let sum = newobj.func1() + newobj.func2() + newobj.func3();
+console.log(sum);
 
 
 
 
 
-!function func4() {
-  console.log('!');
-};
-
-
-
-
--function func5() {
-  console.log('!');
-};
-
-
-
-
-
-1 + function func6() {
-  console.log('!');
-};
-
-
-
-
-
-(function func7() {
-  console.log('!');
-});
-
-
-
-
-
-console.log(
-  function () {
-     console.log('!');
-  }
-);
-
-
-
-
-
-let test = function func() {
-  console.log('!');
-};
-
-
-
-
-console.log(
-  function func() {
-     console.log('!');
-  }
-);
-
-
-
-
-
-+function func11() {
-  console.log('!');
-};
-
-
-
-
-
-function func12() {
-  console.log('!');
+for (let elem in newobj) {
+  console.log(newobj[elem]())
 };
