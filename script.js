b43b95b8ac4be04ab408f08be256fba21bcf383b
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-06-18 22:07:35 +0300

    84

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
+"use strict";
+
 let num = 3;
-
-if ((num > 5 && num < 10) || num == 20) {
-  console.log("+++");
+		
+if (num > 5 && num < 10 || num == 20) {
+	console.log('+++');
 } else {
-  console.log("---");
+	console.log('---');
 }
 
-if (num > 5 || (num > 0 && num < 3)) {
-  console.log("+++");
+
+num = 3;
+		
+if (num > 5 || num > 0 && num < 3) {
+	console.log('+++');
 } else {
-  console.log("---");
+	console.log('---');
 }
 
-if (num == 9 || (num > 10 && num < 20) || (num > 20 && num < 30)) {
-  console.log("+++");
+
+num = 3;
+		
+if (num == 9 || num > 10 && num < 20 || num > 20 && num < 30) {
+	console.log('+++');
 } else {
-  console.log("---");
-}
+	console.log('---');
+} 
