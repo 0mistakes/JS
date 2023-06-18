4c39187a4642412530ce30847f8f9000994ea00b
Vladimir_Oleinik <113135004+0mistakes@users.noreply.github.com>
2023-06-18 22:36:39 +0300

    113

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 "use strict";
 
-for (let i = 1; i <= 100; i++) {
-	console.log(i);
+let obj = {x: 1, y: 2, z: 3};
+for (let key in obj) {
+	document.write(key, "<br>");
 }
 
-for (let i = 11; i <= 33; i++) {
-	console.log(i);
-}
-
-for (let i = 0; i <= 100; i += 2) {
-	console.log(i);
-}
-
-for (let i = 1; i <= 99; i += 2) {
-	console.log(i);
-}
-
-for (let i = 100; i > 0; i--) {
-	console.log(i);
+let obj1 = {x: 1, y: 2, z: 3};
+for (let key in obj1) {
+	document.write(obj1[key]);
 }
